898404efaec1a6bead92cfdd44a0aaa1f4866204
Dimas Muñoz <dimasmunoz@outlook.com>
2021-01-17 23:42:49 +0100

    Fix params

--- run/job_hpc_qa_training.sh
+++ run/job_hpc_qa_training.sh
@@ -2,16 +2,14 @@
 
 source activate tf
 
-python ./run/run_training.py \
-    --model_ckpt ./artifacts/qa_squad/ \
+python ./run/run_qa_training.py \
+    --ckpt_name qa_squad \
     --dataset squad \
     --max_epoches 100 \
-    --total_layers 16 \
     --batch_size 64 \
     --cased 0 \
-    --hidden_dim 100 \
     --lm_name bert-base-multilingual-cased \
-    --lm_emb_dim 768 \
+    --hidden_dim 768 \
     --device cuda \
     --continue_training 0 \
     --log_to_file 1
